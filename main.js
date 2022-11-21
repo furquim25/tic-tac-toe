f6d94042eac07d7d5012bb06b63346a04ce7cdb8
Rafael Furquim <rafa_furquim@nidf.ufrj.br>
2022-11-20 23:37:45 -0300

    added all will conditions

--- main.js
+++ main.js
@@ -9,24 +9,49 @@ function selectPosition(clickedId) {
 }
 
 function checkWin() {
+    winText = "You won!"
+    loseText = "You lost!"
     currentBoard = getBoardArray()
+
     //Check horizontal win conditions
     currentBoard.forEach((item, index) => {
         if(item[0]==item[1] && item[0]==item[2] && item[0] == "X"){
-            document.getElementsByClassName("main-text")[0].innerHTML = "You won!"
-        } else if  (item[0]==item[1] && item[0]==item[2] && item[0] == "O"){
-            document.getElementsByClassName("main-text")[0].innerHTML = "You lost!"
+            if (item[0] == "X"){
+                document.getElementsByClassName("main-text")[0].innerHTML = winText
+            } else if (item[0] == "O"){
+                document.getElementsByClassName("main-text")[0].innerHTML = loseText
+            }
         }
     })
+
     //Check vertical win conditions
     transpose(currentBoard).forEach((item, index) => {
-        if(item[0]==item[1] && item[0]==item[2] && item[0] == "X"){
-            document.getElementsByClassName("main-text")[0].innerHTML = "You won!"
-        } else if  (item[0]==item[1] && item[0]==item[2] && item[0] == "O"){
-            document.getElementsByClassName("main-text")[0].innerHTML = "You lost!"
+        if(item[0]==item[1] && item[0]==item[2]){
+            if (item[0] == "X"){
+                document.getElementsByClassName("main-text")[0].innerHTML = winText
+            } else if (item[0] == "O"){
+                document.getElementsByClassName("main-text")[0].innerHTML = loseText
+            }
         }
     })
-    
+
+    //Check diagonals conditions
+    console.log(currentBoard[0][0])
+    if(currentBoard[0][0]==currentBoard[1][1] && currentBoard[0][0]==currentBoard[2][2]){
+        if (currentBoard[0][0] == "X"){
+            document.getElementsByClassName("main-text")[0].innerHTML = winText
+        } else if (currentBoard[0][0] == "O"){
+            document.getElementsByClassName("main-text")[0].innerHTML = loseText
+        }
+    }
+    if(currentBoard[0][2]==currentBoard[1][1] && currentBoard[0][2]==currentBoard[2][0]){
+        if (currentBoard[0][2] == "X"){
+            document.getElementsByClassName("main-text")[0].innerHTML = winText
+        } else if (currentBoard[0][2] == "O"){
+            document.getElementsByClassName("main-text")[0].innerHTML = loseText
+        }
+    }
+        
 }
 
 function getBoardArray() {
