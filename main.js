8188fd2013cd0843328eab67a495127ab7c0e375
Rafael Furquim <rafa_furquim@nidf.ufrj.br>
2022-11-21 00:37:06 -0300

    disable buttons when winning

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
+isXTurn = false
 function selectPosition(clickedId) {
     clickedElement = document.getElementById(clickedId)
     currentText = clickedElement.textContent.trim()
-    if (currentText == "X" ||  currentText == "O"){
+    if (currentText == "X" || currentText == "O"){
         return
     }
-    clickedElement.textContent = "X"
-    checkWin()
+
+    isXTurn = !isXTurn
+    if (isXTurn){
+        clickedElement.textContent = "X"
+        document.getElementsByClassName("main-text")[0].innerHTML = "It's <b>O</b> turn"
+    } else{
+        clickedElement.textContent = "O"
+        document.getElementsByClassName("main-text")[0].innerHTML = "It's <b>X</b> turn"
+    }
+    checkGameIsOver()
 }
 
-function checkWin() {
-    winText = "You won!"
-    loseText = "You lost!"
+function checkGameIsOver() {
+    winXText = "X won!"
+    winOText = "O won!"
+    tieText = "It's a tie!"
     currentBoard = getBoardArray()
 
     //Check horizontal win conditions
     currentBoard.forEach((item, index) => {
         if(item[0]==item[1] && item[0]==item[2] && item[0] == "X"){
             if (item[0] == "X"){
-                document.getElementsByClassName("main-text")[0].innerHTML = winText
+                document.getElementsByClassName("main-text")[0].innerHTML = winXText
+                disablePlay()
             } else if (item[0] == "O"){
-                document.getElementsByClassName("main-text")[0].innerHTML = loseText
+                document.getElementsByClassName("main-text")[0].innerHTML = winOText
+                disablePlay()
             }
         }
     })
@@ -28,10 +40,13 @@ function checkWin() {
     transpose(currentBoard).forEach((item, index) => {
         if(item[0]==item[1] && item[0]==item[2]){
             if (item[0] == "X"){
-                document.getElementsByClassName("main-text")[0].innerHTML = winText
+                document.getElementsByClassName("main-text")[0].innerHTML = winXText
+                disablePlay()
             } else if (item[0] == "O"){
-                document.getElementsByClassName("main-text")[0].innerHTML = loseText
+                document.getElementsByClassName("main-text")[0].innerHTML = winOText
+                disablePlay()
             }
+            
         }
     })
 
@@ -39,19 +54,34 @@ function checkWin() {
     console.log(currentBoard[0][0])
     if(currentBoard[0][0]==currentBoard[1][1] && currentBoard[0][0]==currentBoard[2][2]){
         if (currentBoard[0][0] == "X"){
-            document.getElementsByClassName("main-text")[0].innerHTML = winText
+            document.getElementsByClassName("main-text")[0].innerHTML = winXText
+            disablePlay()
         } else if (currentBoard[0][0] == "O"){
-            document.getElementsByClassName("main-text")[0].innerHTML = loseText
+            document.getElementsByClassName("main-text")[0].innerHTML = winOText
+            disablePlay()
         }
+        
     }
     if(currentBoard[0][2]==currentBoard[1][1] && currentBoard[0][2]==currentBoard[2][0]){
         if (currentBoard[0][2] == "X"){
-            document.getElementsByClassName("main-text")[0].innerHTML = winText
+            document.getElementsByClassName("main-text")[0].innerHTML = winXText
+            disablePlay()
         } else if (currentBoard[0][2] == "O"){
-            document.getElementsByClassName("main-text")[0].innerHTML = loseText
+            document.getElementsByClassName("main-text")[0].innerHTML = winOText
+            disablePlay()
         }
     }
-        
+
+    //Check tie condition
+    totalFilledSpaces = 0
+    for (i=0; i<=2; i++){
+        totalFilledSpaces += currentBoard[i].filter(Boolean).length
+    }
+    if (totalFilledSpaces == 9){
+        document.getElementsByClassName("main-text")[0].innerHTML = tieText
+        disablePlay()
+    }
+          
 }
 
 function getBoardArray() {
@@ -65,4 +95,13 @@ function getBoardArray() {
 
 function transpose(matrix) {
   return matrix[0].map((col, i) => matrix.map(row => row[i]));
+}
+
+function disablePlay(){
+    //Disable clicks on the board
+    console.log("GAME OVER")
+    for (i=1; i<=9; i++){
+        console.log('pos'+i)
+        document.getElementById('pos'+i).onclick = null
+    }
 }
